1fa2a26af9253e8d34510efda7a50573085b7b50
adiosgandhi <gandhiaditya4545@gmail.com>
2019-06-25 15:35:18 +0530

    first commit

---
+++ prod.js
@@ -0,0 +1,21 @@
+var express=require('express');
+var app=express();
+
+var productcontroller=function(req,res){
+
+console.log("invoking rest api");
+
+var products=[{id:1,title:"ROse",description:"nice flower"},
+{id:2,title:"lily",description:"yellow flower"},
+{id:3,title:"gerbera",description:"wedding flower"}];
+res.send(products);
+};
+
+//routing
+
+app.get('/products',productcontroller);
+var server=app.listen(8086,function(){
+	var host=server.address().address
+	var port=server.address().port
+	console.log("example app listening at port 8086",host,port)
+})
